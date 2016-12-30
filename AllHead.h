85e3685e95bc0dd9931ebf3b4edffe6cee3b3b85
pGlyco <pGlyco@ict.ac.cn>
2016-12-30 15:32:44 +0800

    first_commit

---
+++ AllHead.h
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_EARNING
+#ifndef ALLHEADE_H_
+#define ALLHEADE_H_
+#include "BaseDataStructure.h"
+#include "CalMaxStructure.h"
+#include "GlyTree.h"
+#include "MonoInfo.h"
+#include "ReadData.h"
+#include "Enumerate.h"
+#include"common.h"
+#include "OutputData.h"
+#include <iomanip>
+#include <exception>
+//extern unsigned long long MAXN;
+#endif
